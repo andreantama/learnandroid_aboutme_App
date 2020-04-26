c24f89e3e89c12fdc7b10371745a9d55c1ed197d
andreantama96 <andreanyogatama1@gmail.com>
2020-04-25 21:34:01 -0700

    Membuat action pada button sehingga dapat menampilkan nama nick name yang diisi, It pada listener mengarah paa view id yaitu button, setelah button diclik maka view button akan hilang (view.visibility = View.Gone), menampilkan textview result pada saat button di click dengan (result.visibility = View.VISIBLE)

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    //lateinit
+    lateinit var result: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn_done).setOnClickListener {
+            addNickname(it)
+        }
+        //editText =
+        result = findViewById(R.id.idResult)
+    }
+
+    private fun addNickname(view: View) {
+        var editText:EditText = findViewById(R.id.id_nickname)
+        result.text = editText.text
+        view.visibility = View.GONE
+        result.visibility = View.VISIBLE
     }
 }
